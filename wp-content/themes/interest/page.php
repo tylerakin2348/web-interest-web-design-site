0e4be20e3d7690e50f3aafc7e4c10573d40241f2
tylerakin2348 <takin963@students.sbts.edu>
2017-01-05 18:25:04 -0500

    fixed styling error on about section of page

--- wp-content/themes/interest/page.php
+++ wp-content/themes/interest/page.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
+  <!-- <div class="page"> -->
 
-  <?php
-	if ( have_posts() ) : while ( have_posts() ) : the_post();
+    <?php
 
-    get_template_part( 'content', get_post_format() );
+  	if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-  endwhile; endif;
-  ?>
+      get_template_part( 'content', get_post_format() );
 
+    endwhile; endif;
+    ?>
+  <!-- </div> -->
 <?php get_footer(); ?>
